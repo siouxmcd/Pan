63fdae4d54c8bdff097655133c11e27aad89c1aa
siouxmcd <claeomcdermott@u.boisestate.edu>
2017-09-29 21:29:33 -0600

    Initial creation and testing
    Added Unity Asset for basing code off of, started new script

---
+++ Pan/Assets/MakeTree.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeTree : MonoBehaviour {
+    public int time = 2;
+    public double minDistance = 0.1;
+    public bool autoCalculateOrientation = true;
+
+    private bool invertFaces = false;
+    private Mesh srcMesh;
+    private MeshExtrusion.Edge[] precomputedEdges;
+    class ExtrudedTrunk
+    {
+        Vector3 point;
+        Matrix4x4 matrix;
+        float time;
+    }
+
+	// Use this for initialization
+	void Start () {
+        srcMesh = GetComponent<MeshFilter>().sharedMesh;
+        precomputedEdges = MeshExtrusion.BuildManifoldEdges(srcMesh);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+         
+	}
+}
