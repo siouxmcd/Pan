d811e2435acde4fd88ccaa66719a0514ecc0ccf0
siouxmcd <claeomcdermott@u.boisestate.edu>
2017-09-30 14:24:55 -0600

    Broken extrusion
    but it does something

--- Pan/Assets/MakeTree.cs
+++ Pan/Assets/MakeTree.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class MakeTree : MonoBehaviour {
     public int time = 2;
@@ -10,11 +11,15 @@ public class MakeTree : MonoBehaviour {
     private bool invertFaces = false;
     private Mesh srcMesh;
     private MeshExtrusion.Edge[] precomputedEdges;
+    //private ArrayList sections = new ArrayList();
+    //private object[] sections = new object[1];
+    //List<object> sections = new List<object>();
+    List<ExtrudedTrunk> sections = new List<ExtrudedTrunk>();
     class ExtrudedTrunk
     {
-        Vector3 point;
-        Matrix4x4 matrix;
-        float time;
+        public Vector3 point;
+        public Matrix4x4 matrix;
+        public float time;
     }
 
 	// Use this for initialization
@@ -22,9 +27,66 @@ public class MakeTree : MonoBehaviour {
         srcMesh = GetComponent<MeshFilter>().sharedMesh;
         precomputedEdges = MeshExtrusion.BuildManifoldEdges(srcMesh);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-         
+        Vector3 pos = transform.position;
+        float now = Time.time;
+
+        if (sections.Count == 0 || (sections[0].point - pos).sqrMagnitude > minDistance * minDistance)
+        {
+            ExtrudedTrunk section = new ExtrudedTrunk();
+            section.point = pos;
+            section.matrix = transform.localToWorldMatrix;
+            section.time = now;
+            sections.Insert(0, section);
+        }
+
+        if (sections.Count < 2)
+            return;
+
+        Matrix4x4 worldToLocal = transform.worldToLocalMatrix;
+        Matrix4x4[] finalSections = new Matrix4x4[sections.Count];
+        Quaternion previousRotation;
+
+        int i;
+        for (i=0; i < sections.Count; i++)
+        {
+            if (autoCalculateOrientation)
+            {
+                if (i == 0)
+                {
+                    Vector3 direction = sections[0].point - sections[1].point;
+                    Quaternion rot = Quaternion.LookRotation(direction, Vector3.up);
+                    previousRotation = rot;
+                    finalSections[i] = worldToLocal * Matrix4x4.TRS(pos, rot, Vector3.one);
+                }
+                else if (i != sections.Count - 1)
+                {
+                    Vector3 direction = sections[0].point - sections[1].point;
+                    Quaternion rot = Quaternion.LookRotation(direction, Vector3.up);
+                    previousRotation = rot;
+                    finalSections[i] = worldToLocal * Matrix4x4.TRS(pos, rot, Vector3.one);
+                    if (Quaternion.Angle(previousRotation, rot) > 20)
+                        rot = Quaternion.Slerp(previousRotation, rot, 0.5f);  
+                }
+                else
+                {
+                    finalSections[i] = finalSections[i - 1];
+                }
+            }
+            else
+            {
+                if(i == 0)
+                {
+                    finalSections[i] = Matrix4x4.identity;
+                }
+                else
+                {
+                    finalSections[i] = worldToLocal * sections[i].matrix;
+                }
+            }
+        }
+        MeshExtrusion.ExtrudeMesh(srcMesh, GetComponent<MeshFilter>().mesh, finalSections, precomputedEdges, invertFaces);
 	}
 }
---
+++ Pan/Assets/rigidBodyVelocity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigidBodyVelocity : MonoBehaviour {
+    public int speed = 10;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward) * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
