efb9a99b917381f1e45d2c469b87c5dfc587ca98
siouxmcd <claeomcdermott@u.boisestate.edu>
2017-10-01 08:51:25 -0600

    More changes

---
+++ Pan/Assets/FlutePlay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlutePlay : MonoBehaviour {
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "pipe1")
+        {
+            Debug.Log("found pipe 1");
+        }
+        else if (other.gameObject.tag == "pipe2")
+        {
+            Debug.Log("found pipe 2");
+        }
+        else if (other.gameObject.tag == "pipe3")
+        {
+            Debug.Log("found pipe 3");
+        }
+        else if (other.gameObject.tag == "pipe4")
+        {
+            Debug.Log("found pipe 4");
+        }
+        else if (other.gameObject.tag == "pipe5")
+        {
+            Debug.Log("found pipe 5");
+        }
+        else if (other.gameObject.tag == "pipe6")
+        {
+            Debug.Log("found pipe 6");
+        }
+        else if (other.gameObject.tag == "pipe7")
+        {
+            Debug.Log("found pipe 7");
+        }
+        else if (other.gameObject.tag == "pipe8")
+        {
+            Debug.Log("found pipe 8");
+        }
+    }
+}
--- Pan/Assets/MakeTree.cs
+++ Pan/Assets/MakeTree.cs
@@ -6,9 +6,9 @@ using System.Linq;
 public class MakeTree : MonoBehaviour {
     public int time = 2;
     public double minDistance = 0.1;
-    public float growFactor = 0.1f;
     public bool autoCalculateOrientation = true;
 
+    private Vector3 newPos;
     private bool invertFaces = false;
     private Mesh srcMesh;
     private MeshExtrusion.Edge[] precomputedEdges;
@@ -19,7 +19,6 @@ public class MakeTree : MonoBehaviour {
     class ExtrudedTrunk
     {
         public Vector3 point;
-        public Vector3 s;
         public Matrix4x4 matrix;
         public float time;
     }
@@ -28,23 +27,20 @@ public class MakeTree : MonoBehaviour {
 	void Start () {
         srcMesh = GetComponent<MeshFilter>().sharedMesh;
         precomputedEdges = MeshExtrusion.BuildManifoldEdges(srcMesh);
-	}
+    }
 
 	// Update is called once per frame
 	void Update () {
         Vector3 pos = transform.position;
-        Vector3 scale = transform.localScale;
         float now = Time.time;
 
         if (sections.Count == 0 || (sections[0].point - pos).sqrMagnitude > minDistance * minDistance)
         {
             ExtrudedTrunk section = new ExtrudedTrunk();
             section.point = pos;
-            section.s = scale;
             section.matrix = transform.localToWorldMatrix;
             section.time = now;
-            sections.Add(section);
-            scaleTree();
+            sections.Insert(0, section);
         }
 
         if (sections.Count < 2)
@@ -56,24 +52,26 @@ public class MakeTree : MonoBehaviour {
         
 
         int i;
+        float xp = Random.Range(-0.1f, 0.1f);
+        float zp = Random.Range(-0.1f, 0.1f);
         for (i=0; i < sections.Count; i++)
         {
-            if (autoCalculateOrientation)
-            {
-                if (i == 0)
-                {
-                    Vector3 direction = sections[0].point - sections[1].point;
-                    Quaternion rot = Quaternion.LookRotation(direction, Vector3.up);
-                    previousRotation = rot;
-                    finalSections[i] = worldToLocal * Matrix4x4.TRS(pos, rot, Vector3.one);
-                }
-                else
-                {
-                    finalSections[i] = finalSections[i - 1];
-                }
-            }
-            else
-            {
+            //if (autoCalculateOrientation)
+            //{
+            //    if (i == 0)
+            //    {
+            //        Vector3 direction = sections[0].point - sections[1].point;
+            //        Quaternion rot = Quaternion.LookRotation(direction, Vector3.up);
+            //        previousRotation = rot;
+            //        finalSections[i] = worldToLocal * Matrix4x4.TRS(pos, rot, Vector3.one);
+            //    }
+            //    else
+            //    {
+            //        finalSections[i] = finalSections[i - 1];
+            //    }
+            //}
+            //else
+            //{
                 if(i == 0)
                 {
                     finalSections[i] = Matrix4x4.identity;
@@ -81,23 +79,11 @@ public class MakeTree : MonoBehaviour {
                 else
                 {
                     finalSections[i] = worldToLocal * sections[i].matrix;
-                    sections[i].s += new Vector3(1, 0, 1) * now;
+                
                 }
-            }
+            transform.position += new Vector3(xp, 0, zp) * Time.deltaTime / 25;
+            //}
         }
         MeshExtrusion.ExtrudeMesh(srcMesh, GetComponent<MeshFilter>().mesh, finalSections, precomputedEdges, invertFaces);
 	}
-    void scaleTree() {
-        int i;
-        int j;
-        for (i = 0; i < sections.Count; i++)
-        {
-            j = 0;
-            while (j < 200)
-            {
-                sections[i].point += new Vector3(1, 0, 1) * Time.deltaTime;
-                j++;
-            }
-        }
-    }
 }
